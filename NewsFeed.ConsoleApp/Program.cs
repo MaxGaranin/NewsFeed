1ecaa5d06fb51831520b95c44889bdeadf1dcd50
GaraninMS <maxgaranin@gmail.com>
2019-06-01 00:19:00 +0400

    Подключение к базе через консольное приложение

--- NewsFeed.ConsoleApp/Program.cs
+++ NewsFeed.ConsoleApp/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NewsFeed.DataAccess;
+using NewsFeed.Model.Entities;
 
 namespace NewsFeed.ConsoleApp
 {
@@ -6,7 +11,46 @@ namespace NewsFeed.ConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<NewsFeedDbContext>();
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlite(connectionString);
+
+            using (var context = new NewsFeedDbContext(optionsBuilder.Options))
+            {
+                context.Database.Migrate();
+
+                var user = new User
+                {
+                    Name = "Max",
+                    SurName = "Garanin",
+                    Nick = "MaG"
+                };
+
+                context.Users.Add(user);
+
+                var newsFeedItem = new NewsFeedItem
+                {
+                    Author = user,
+                    Content = "Hello, world",
+                    PublishDate = DateTime.Now
+                };
+
+                context.NewsFeedItems.Add(newsFeedItem);
+
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
     }
 }
